5a3c20868950ce335e0d0525ed5051dd12ec4208
yxl1014 <1627419257@qq.com>
2022-10-17 15:25:29 +0800

    TestApp_V2-MainServer LruConfig

--- src/main/java/main/ly/config/LruConfig.java
+++ src/main/java/main/ly/config/LruConfig.java
@@ -1,6 +1,7 @@
 package main.ly.config;
 
 import main.ly.config.LruBean.LruUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pto.TestProto;
@@ -8,8 +9,11 @@ import pto.TestProto;
 @Configuration
 public class LruConfig {
 
+    @Value("${lru.maxsize}")
+    private int capacity;
+
     @Bean
-    public LruUtil<TestProto.Task.Builder> getLruList(int capacity){
+    public LruUtil<TestProto.Task.Builder> getLruList() {
         return new LruUtil<>(capacity);
     }
 }
